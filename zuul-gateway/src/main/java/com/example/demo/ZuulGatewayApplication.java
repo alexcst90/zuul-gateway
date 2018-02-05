49d6780db424cda810ec0dd1fbbde74cce6da0db
Alexander <a.cmejia10@gmail.com>
2018-02-05 17:02:40 -0600

    primero

---
+++ zuul-gateway/src/main/java/com/example/demo/ZuulGatewayApplication.java
@@ -0,0 +1,23 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.web.ErrorMvcAutoConfiguration;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan
+@EnableAutoConfiguration(exclude = {ErrorMvcAutoConfiguration.class})
+@EnableDiscoveryClient
+@EnableZuulProxy
+@SpringBootApplication
+public class ZuulGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZuulGatewayApplication.class, args);
+	}
+}
